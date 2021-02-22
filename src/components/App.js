a4a5b5571d1204653c15cc63a792f064ef3ec77e
kranuj3122 <kumaranuj22799@gmail.com>
2021-02-23 03:14:47 +0530

    hello

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import "../styles/App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
   let [time, setTime] = useState(new Date().toLocaleTimeString());
-  const updateTime=()=>{
-      let currTime=new Date().toLocaleTimeString();
-      setTime(currTime);
+  const updateTime = () => {
+    let currTime = new Date().toLocaleTimeString();
+    setTime(currTime);
   };
-  setInterval(updateTime,1000);
+  useEffect(() => {
+    let timer = setInterval(updateTime, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [time]);
   return (
     <div className="Clock">
       <h3 id="time">{time}</h3>
