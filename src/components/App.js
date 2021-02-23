fa0b911c4644f88c14f23fb2992f45fd55ffb16c
kranuj3122 <kumaranuj22799@gmail.com>
2021-02-23 14:09:35 +0530

    class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,54 @@
 import "../styles/App.css";
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  let [time, setTime] = useState(new Date().toLocaleTimeString());
-  const updateTime = () => {
-    let currTime = new Date().toLocaleTimeString();
-    setTime(currTime);
-  };
-  useEffect(() => {
-    let timer = setInterval(()=>{
-      updateTime();
-    }, 1000);
-    return () => {
-      clearInterval(timer);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      time: new Date().toLocaleTimeString(),
     };
-  }, [time]);
-  return (
-    <div className="Clock">
-      <h3 id="time">{time}</h3>
-    </div>
-  );
-};
+  }
+  update() {
+    this.setState({ time: new Date().toLocaleTimeString() });
+  }
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.update();
+    }, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">{this.state.time}</h3>
+      </div>
+    );
+  }
+}
 
 export default App;
+
+// const App = () => {
+//   let [time, setTime] = useState(new Date().toLocaleTimeString());
+//   const updateTime = () => {
+//     let currTime = new Date().toLocaleTimeString();
+//     setTime(currTime);
+//   };
+//   useEffect(() => {
+//     let timer = setInterval(()=>{
+//       updateTime();
+//     }, 1000);
+//     return () => {
+//       clearInterval(timer);
+//     };
+//   }, [time]);
+//   return (
+//     <div className="Clock">
+//       <h3 id="time">{time}</h3>
+//     </div>
+//   );
+// };
+
+// export default App;
